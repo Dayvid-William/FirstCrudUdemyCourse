c183d2147f68151e0b646568ce4f753c15e500a3
Dayvid-William <dayvid.william5@outlook.com>
2023-05-16 22:01:34 -0300

    end CRUD

--- src/main/java/com/test/primeiroexemplo/services/ProdutoService.java
+++ src/main/java/com/test/primeiroexemplo/services/ProdutoService.java
@@ -9,36 +9,38 @@ import com.test.primeiroexemplo.model.Produto;
 import com.test.primeiroexemplo.repository.ProdutoRepository;
 
 @Service
-public class ProdutoServices {
+public class ProdutoService {
   
   @Autowired
-  private ProdutoRepository produtorepository;
+  private ProdutoRepository produtoRepository;
 
-   /**
+  /**
    * Metodo para retorna uma lista de produtos
    * @return lista de produtos.
    */
   public List<Produto> obterTodos(){
-    return produtorepository.obterTodos();
+    //Colocar Regra aqui caso tenha...
+    return produtoRepository.obterTodos();
   }
 
-    /**
+  
+  /**
    * Metodo que retorna o produto encontrado pelo seu Id.
    * @param id do produto que será localizado.
    * @return Retorna um produto caso seja encontrado
    */
   public Optional<Produto> obterPorId(int id) {
-    return produtorepository.obterPorId(id);
+    return produtoRepository.obterPorId(id);
   }
 
-   /**
+  /**
    * Metodo para adicionar produto na lista.
    * @param produto que será adicionado.
    * @return Retorna produto que foi adicionado na lista.
    */
   public Produto adicionar(Produto produto) {
-    // Poderia ter alguma regra de negocio aqui para validar o produto.
-    return produtorepository.adicionar(produto);
+    // Poderia ter alguma regra de negocio para validar o produto.
+    return produtoRepository.adicionar(produto);
   }
 
   /**
@@ -46,18 +48,21 @@ public class ProdutoServices {
    * @param id do produto que será deletado.
    */
   public void deletar(int id) {
-    produtorepository.deletar(id);
+    //Poderia ter alguma logica de validação
+    produtoRepository.deletar(id);
   }
 
   /**
    * Metodo para atualizar um produto na lista.
    * @param produto que será atualizado.
+   * @param id do produto
    * @return Retorna o produto atualizado na lista.
    */
   public Produto atualizar(int id, Produto produto) {
-    //Ter validação no id
-    produto.setId(id);
     
-    return produtorepository.atualizar(produto);
+    produto.setId(id);
+
+    return produtoRepository.atualizar(produto);
   }
+
 }
