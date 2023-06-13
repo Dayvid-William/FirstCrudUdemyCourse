5a2f3bff6dd9cddf39c212ee991254554a7d2c28
Dayvid-William <dayvid.william5@outlook.com>
2023-06-13 19:33:27 -0300

    connect finally with sucessful

--- src/main/java/com/test/primeiroexemplo/model/Produto.java
+++ src/main/java/com/test/primeiroexemplo/model/Produto.java
@@ -1,8 +1,16 @@
 package com.test.primeiroexemplo.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Produto {
   
   //#region Atributos
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
   private int id;
 
   private String nome;
