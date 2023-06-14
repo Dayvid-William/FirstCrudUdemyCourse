156521c82005d6b895ce6a2a6bc1794d98774242
Dayvid-William <dayvid.william5@outlook.com>
2023-06-13 23:47:07 -0300

    started the DTO concept

--- src/main/java/com/test/primeiroexemplo/services/ProdutoService.java
+++ src/main/java/com/test/primeiroexemplo/services/ProdutoService.java
@@ -2,11 +2,16 @@ package com.test.primeiroexemplo.services;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.test.primeiroexemplo.model.Produto;
+import com.test.primeiroexemplo.model.exception.ResourceNotFoundException;
 import com.test.primeiroexemplo.repository.ProdutoRepository;
+import com.test.primeiroexemplo.shared.ProdutoDTO;
 
 @Service
 public class ProdutoService {
@@ -18,9 +23,15 @@ public class ProdutoService {
    * Metodo para retorna uma lista de produtos
    * @return lista de produtos.
    */
-  public List<Produto> obterTodos(){
+  public List<ProdutoDTO> obterTodos(){
     //Colocar Regra aqui caso tenha...
-    return produtoRepository.findAll();
+
+    //Retorna uma lista de produtos model.
+    List<Produto> produtos = produtoRepository.findAll();
+
+    return produtos.stream()
+    .map(produto -> new ModelMapper().map(produto, ProdutoDTO.class))
+    .collect(Collectors.toList());
   }
 
   
@@ -29,8 +40,17 @@ public class ProdutoService {
    * @param id do produto que será localizado.
    * @return Retorna um produto caso seja encontrado
    */
-  public Optional<Produto> obterPorId(int id) {
-    return produtoRepository.findById(id);
+  public Optional<ProdutoDTO> obterPorId(int id) {
+    //Obtendo optional de produto por id.
+    Optional<Produto> produto = produtoRepository.findById(id);
+    //Se não encontra, lança exception.
+    if(produto.isEmpty()){
+      throw new ResourceNotFoundException("Produto com id: " + id + " não encontrado");
+    }
+    //Convertendo o meu optional de produto em um produto dto.
+    ProdutoDTO dto = new ModelMapper().map(produto.get(), ProdutoDTO.class);
+    //Criando e retornando um optional de produto dto.
+    return Optional.of(dto);
   }
 
   /**
@@ -38,8 +58,10 @@ public class ProdutoService {
    * @param produto que será adicionado.
    * @return Retorna produto que foi adicionado na lista.
    */
-  public Produto adicionar(Produto produto) {
-    // Poderia ter alguma regra de negocio para validar o produto.
+  public ProdutoDTO adicionar(ProdutoDTO produtoDTO) {
+    // Removendo o id para conseguir fazer cadastro.
+
+
     return produtoRepository.save(produto);
   }
 
@@ -58,11 +80,11 @@ public class ProdutoService {
    * @param id do produto
    * @return Retorna o produto atualizado na lista.
    */
-  public Produto atualizar(int id, Produto produto) {
+  public ProdutoDTO atualizar(int id, ProdutoDTO produto) {
     
     produto.setId(id);
 
-    return produtoRepository.saveAndFlush(produto);
+    return produtoRepository.save(produto);
   }
 
 }
