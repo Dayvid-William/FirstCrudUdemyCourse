0d4021ba1cdc511fb6cdff26725fc2a7af43e297
Dayvid-William <dayvid.william5@outlook.com>
2023-06-15 12:27:51 -0300

    finalizing the configuration of the use of the DTO

--- src/main/java/com/test/primeiroexemplo/shared/ProdutoDTO.java
+++ src/main/java/com/test/primeiroexemplo/shared/ProdutoDTO.java
@@ -13,11 +13,11 @@ public class ProdutoDTO {
   private String observacao;
 
   //#region GettersAndSetters
-  public int getId() {
+  public Integer getId() {
     return id;
   }
 
-  public void setId(int id) {
+  public void setId(Integer id) {
     this.id = id;
   }
 
--- src/main/java/com/test/primeiroexemplo/view/controller/ProdutoController.java
+++ src/main/java/com/test/primeiroexemplo/view/controller/ProdutoController.java
@@ -8,6 +8,7 @@ import java.util.stream.Collectors;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,6 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.test.primeiroexemplo.model.Produto;
 import com.test.primeiroexemplo.services.ProdutoService;
 import com.test.primeiroexemplo.shared.ProdutoDTO;
+import com.test.primeiroexemplo.view.model.ProdutoRequest;
 import com.test.primeiroexemplo.view.model.ProdutoResponse;
 
 @RestController
@@ -47,9 +49,9 @@ public class ProdutoController {
   public ResponseEntity<Optional<ProdutoResponse>> obterPorId(@PathVariable int id) {
     //try {
       //transforma o Produto model em ProdutoDTO
-    Optional<ProdutoDTO> dto = produtoService.obterPorId(id);
+    Optional<ProdutoDTO> produtoDto = produtoService.obterPorId(id);
     //transforma o produto DTO em ProdutoResponse
-    ProdutoResponse produto = new ModelMapper().map(dto.get(), ProdutoResponse.class);
+    ProdutoResponse produto = new ModelMapper().map(produtoDto.get(), ProdutoResponse.class);
     //Retorna um Optional de produto response com status http Ok
     return new ResponseEntity<>(Optional.of(produto), HttpStatus.OK);
     //} catch (Exception e) {
@@ -59,18 +61,36 @@ public class ProdutoController {
   }
 
   @PostMapping
-  public Produto adicionar(@RequestBody Produto produto) {
-    return produtoService.adicionar(produto);
+  public ResponseEntity<ProdutoResponse> adicionar(@RequestBody ProdutoRequest produtoReq) {
+    ModelMapper mapper = new ModelMapper();
+    //converte o produto request em produto dto
+    ProdutoDTO produtoDto = mapper.map(produtoReq, ProdutoDTO.class);
+    //Adiciona o produto dto no banco de dados
+    produtoDto = produtoService.adicionar(produtoDto);
+    //retorna com response entity para poder retorna o status de CREATED e converte o produto adicionado em produto response para retorna ao usuario
+    return new ResponseEntity<>(mapper.map(produtoDto, ProdutoResponse.class), HttpStatus.CREATED);
   }
 
   @DeleteMapping("/{id}")
-  public String deletar(@PathVariable int id) {
+  //Retorna um response entity de qualquer coisa (coringa)
+  public ResponseEntity<?> deletar(@PathVariable Integer id) {
+    //Deleta o produto por id
     produtoService.deletar(id);
-    return "Produto com id:" + id + " foi deletado com sucesso !";
+    //Retorna um response entity com status de no content (informando assim que o produto de id especifico foi deletado)
+    return new ResponseEntity<>(HttpStatus.NO_CONTENT);
   }
 
   @PutMapping("/{id}")
-  public Produto atualizar(@RequestBody Produto produto, @PathVariable int id){
-    return produtoService.atualizar(id, produto);
+  public ResponseEntity<ProdutoResponse> atualizar(@RequestBody ProdutoRequest produtoReq, @PathVariable int id){
+    //Cria o model mapper ("conversor")
+    ModelMapper mapper = new ModelMapper();
+    //Converte o produto request em produto Dto 
+    ProdutoDTO produtoDto = mapper.map(produtoReq, ProdutoDTO.class);
+    //Atribui ao produto DTO o produto que está vindo de resposta do metodo atualizar vindo de ProdutoService 
+    produtoDto = produtoService.atualizar(id, produtoDto);
+    //Converte o produto dto em produto response e envia o status Http ok
+    return new ResponseEntity<>(
+    mapper.map(produtoDto, ProdutoResponse.class),
+    HttpStatus.OK);
   }
 }
